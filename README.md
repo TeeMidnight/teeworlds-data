b0353403fb03b26278d1b5d711e470abb279c615
Bamcane <diandaokui@qq.com>
2024-12-21 22:06:38 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Teeworlds-Data
+==============
+一个仓库用于自动更新一些teeworlds相关资源
+
+# 提供列表
+DDNet(Android)
+TaterClient(Windows)
---
+++ cache.py
@@ -0,0 +1,61 @@
+import requests, json, sys, os
+
+new_files = []
+def CreateDirIfNotExists(dirpath):
+    if os.path.exists(dirpath) == False:
+        os.mkdir(dirpath)
+
+def FastDownloadFile(url, save_path):
+    response = requests.get(url, allow_redirects=True, stream=True)
+    size = response.headers.get("content-length")
+
+    file = open(save_path, "wb")
+    count = 1
+    for i in response.iter_content(chunk_size=1024):
+        file.write(i)
+        print(f"下载中..{round(count * 1024 / float(size) * 100, 2)}%", end="\r")
+        count += 1
+    print(f"下载完成", end="\r")
+    new_files.append(save_path)
+
+def FetchDDNet():
+    try:
+        url = "https://update.ddnet.org/update.json"
+        update = requests.get(url, allow_redirects=True)
+        update_json = json.loads(update.content) #获取版本号
+        
+        Version = str(update_json[0]["version"])
+        FileCheckName = f"cache/DDNet/DDNet-{Version}.apk"
+        if os.path.exists(FileCheckName) == False:
+            print(f"正在拉取DDNet-Android-{Version}")
+            CreateDirIfNotExists("cache/DDNet")
+            FastDownloadFile(f"https://ddnet.org/downloads/DDNet-{Version}.apk", FileCheckName)
+
+        print("DDNet已更新到最新!")
+    except:
+        print("拉取DDNet部分失败!")
+
+def FetchTaterClient():
+    try:
+        url = "https://api.github.com/repos/sjrc6/TaterClient-ddnet/releases/latest"
+        update = requests.get(url, allow_redirects=True)
+        update_json = json.loads(update.content) #获取版本号
+        
+        Version = str(update_json["name"])
+        FileCheckName = f"cache/TaterClient/TaterClient-{Version}.zip"
+        if os.path.exists(FileCheckName) == False:
+            print(f"正在拉取TaterClient-{Version}")
+            CreateDirIfNotExists("cache/TaterClient")
+            FastDownloadFile(f"https://github.com/sjrc6/TaterClient-ddnet/releases/download/{update_json["tag_name"]}/TClient-windows.zip", f"cache/TaterClient/TaterClient-{Version}.zip")
+
+        print("TaterClient已更新到最新!")
+    except:
+        print("拉取TaterClient部分失败!")
+
+def main():
+    CreateDirIfNotExists("cache")
+    FetchDDNet()
+    FetchTaterClient()
+
+if __name__ == "__main__":
+    sys.exit(main())
